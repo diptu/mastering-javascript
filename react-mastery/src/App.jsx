913897ba2ccdab2afc18a0300468ab001c9473aa
diptu <diptunazmulalam@gmail.com>
2026-05-03 20:16:42 +0600

    Feat(day01): Creating child components

---
+++ react-mastery/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ProductDetails from './components/ProductDetails'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <ProductDetails></ProductDetails>
+    </>
+  )
+}
+
+export default App
---
+++ react-mastery/src/components/ProductDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductDetails = () => {
+    return (
+        <div>
+            <h1>Lenovo IdeaPad Slim 3 15IAN8 Core i3 N305 15.6" FHD Laptop</h1>
+            <p>The Lenovo IdeaPad Slim 3 15IAN8 82XB00DXLK is a sleek and reliable 15.6-inch Full HD laptop powered by the efficient Intel Core i3-N305 processor, which features 8 cores and 8 threads with speeds of up to 3.8 GHz. Designed for smooth multitasking, it comes equipped with 8GB LPDDR5-4800 RAM and a fast 512GB M.2 PCIe 4.0x4 NVMe SSD, ensuring quick boot times and ample storage. The anti-glare TN display offers a crisp 1920x1080 resolution and 250nits brightness, complemented by an 88% screen-to-body ratio for immersive viewing. Integrated Intel UHD Graphics handle everyday visuals, while Dolby Audio-enhanced stereo speakers and dual-array microphones deliver clear sound for video calls and entertainment. Connectivity is seamless with Wi-Fi 6, Bluetooth 5.2, USB-C 3.2 Gen 1, HDMI 1.4, and multiple USB ports. Built with durable PC-ABS material and tested to MIL-STD-810H standards, this laptop also includes a privacy shutter for its HD webcam and firmware TPM 2.0 for added security. Ideal for students, professionals, and casual users, the Lenovo IdeaPad Slim 3 15IAN8 combines performance, portability, and value in one stylish package.</p>
+            <p>Price: 55,500৳</p>
+            <img src="https://www.startech.com.bd/image/cache/catalog/laptop/lenovo/ideapad-slim-3i-15ian8/ideapad-slim-3-15ian8-02-500x500.webp" />
+        </div>
+    )
+}
+
+export default ProductDetails
