b2e3365ff4fe11e3c8413c1aaa97e03d1854fb4a
diptu <diptunazmulalam@gmail.com>
2026-05-04 23:39:55 +0600

    feat(day02):Dynamic styling and passing data through props.

--- react-mastery/src/App.jsx
+++ react-mastery/src/App.jsx
@@ -1,6 +1,56 @@
 import { useState } from 'react'
 import ProductDetails from './components/ProductDetails'
 import './App.css'
+import milkImg from './images/fresh-milk.png';
+import cheeseImg from './images/cottage-cheese.png';
+import brocoliImg from './images/brocoli.png';
+import orangesImg from './images/oranges.png';
+import oliveoilImg from './images/olive-oil.png';
+
+
+
+let products = [
+  {
+    pID: 1,
+    pName: 'Fresh Milk',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: milkImg,
+    price: 12
+  },
+  {
+    pID: 2,
+    pName: 'Cottage Cheese',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: false,
+    image: cheeseImg,
+    price: 10
+  },
+  {
+    pID: 3,
+    pName: 'Brocoli',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: brocoliImg,
+    price: 15
+  },
+  {
+    pID: 4,
+    pName: 'oranges',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: orangesImg,
+    price: 20
+  },
+  {
+    pID: 5,
+    pName: 'Olive oil',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: false,
+    image: oliveoilImg,
+    price: 14
+  }
+]
 
 function App() {
 
@@ -9,7 +59,23 @@ function App() {
     <>
       <div className='row'>
         <div>
-          <ProductDetails></ProductDetails>
+          <ProductDetails
+            id={products[0].pID}
+            name={products[0].pName}
+            description={products[0].desc}
+            imgUrl={products[0].image}
+            price={products[0].price}
+
+          >
+          </ProductDetails>
+          <ProductDetails
+            id={products[1].pID}
+            name={products[1].pName}
+            description={products[1].desc}
+            imgUrl={products[1].image}
+            price={products[1].price}
+          ></ProductDetails>
+
         </div>
       </div>
     </>
--- react-mastery/src/components/ProductDetails.jsx
+++ react-mastery/src/components/ProductDetails.jsx
@@ -1,24 +1,34 @@
+// 1. Import the image at the top
+import productImg from "../images/ideapad-slim-3-15.webp";
+let style = {
+    padding: '0px 20px',
+    fontSize: 12
+}
 let Productcount = 5
+let isAvailable = 'Available'
 
+let badgeClass = 'badge-margin-left-80 badge '
+badgeClass += isAvailable === 'Available' ? 'bg-success' : 'bg-danger'
 function DisplayProductCount() {
     return Productcount > 0 ? Productcount : 'Zero';
 }
-const ProductDetails = () => {
+const ProductDetails = ({ id, name, description, imgUrl, price }) => {
     return (
         <ul class="list-group shadow">
             <li class="list-group-item">
                 <div class="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                     <div class="media-body order-2 order-lg-1">
-                        <h5 class="mt-0 font-weight-bold mb-2">Lenovo IdeaPad Slim 3 15IAN8 Core i3 N305 15.6" FHD Laptop</h5>
-                        <p class="font-italic text-muted mb-0 small">The Lenovo IdeaPad Slim 3 15IAN8 82XB00DXLK is a sleek and reliable 15.6-inch Full HD laptop powered by the efficient Intel Core i3-N305 processor, which features 8 cores and 8 threads with speeds of up to 3.8 GHz. Designed for smooth multitasking, it comes equipped with 8GB LPDDR5-4800 RAM and a fast 512GB M.2 PCIe 4.0x4 NVMe SSD, ensuring quick boot times and ample storage. The anti-glare TN display offers a crisp 1920x1080 resolution and 250nits brightness, complemented by an 88% screen-to-body ratio for immersive viewing</p>
+                        <h5 class="mt-0 font-weight-bold mb-2">{name}</h5>
+                        <p class="font-italic text-muted mb-0 small">{description}</p>
                         <div class="d-flex align-items-center justify-content-start mt-1">
-                            <h6 class="font-weight-bold my-2">Price	: 55,500৳</h6>
+                            <h6 class="font-weight-bold my-2">Price	: ${price}</h6>
                             <button className='btn btn-primary'>-</button>
-                            <span>{DisplayProductCount()}</span>
+                            <span style={style}>{DisplayProductCount()}</span>
                             <button className='btn btn-primary'>+</button>
+                            <span className={badgeClass}>{isAvailable}</span>
 
                         </div>
-                    </div><img src="https://www.startech.com.bd/image/cache/catalog/laptop/lenovo/ideapad-slim-3i-15ian8/ideapad-slim-3-15ian8-02-500x500.webp" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
+                    </div><img src={imgUrl} alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
                 </div>
             </li>
         </ul>
