f3d41773508396ce83021a62d0d2f5ac69644e46
diptu <diptunazmulalam@gmail.com>
2026-05-10 23:21:58 +0600

    feat(day-05):Form submission & 2 way data-binding

--- react-mastery/src/components/createProduct/ProductForm.jsx
+++ react-mastery/src/components/createProduct/ProductForm.jsx
@@ -1,31 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CreateProduct from './CreateProduct'
 
 const ProductForm = () => {
+
+
+    let [userInput, updateUserInput] = useState(
+        {
+            pName: '',
+            pPrice: 0,
+            pDescription: '',
+            pAvailability: true,
+            pImage: ''
+        }
+    )
+
+
     let nameInputHandler = (event) => {
-        console.log(event.target.value)
+
+        updateUserInput((prevState) => {
+            return {
+                ...prevState,
+                pName: event.target.value
+            }
+        })
+
     }
     let priceInputHandler = (event) => {
-        console.log(event.target.value)
+
+        updateUserInput((prevState) => {
+            return {
+                ...prevState,
+                pPrice: event.target.value
+            }
+        })
+
+
     }
     let descriptionInputHandler = (event) => {
-        console.log(event.target.value)
+        updateUserInput((prevState) => {
+            return {
+                ...prevState,
+                pDescription: event.target.value
+            }
+        })
     }
     let availabilityInputHandler = (event) => {
-        console.log(event.target.value)
+        updateUserInput((prevState) => {
+            return {
+                ...prevState,
+                pAvailability: event.target.checked
+            }
+        })
     }
 
     let imageInputHandler = (event) => {
-        console.log(event.target.value)
+        updateUserInput((prevState) => {
+            return {
+                ...prevState,
+                pImage: event.target.value
+            }
+        })
+    }
+
+    const CreateProductEventHandler = (event) => {
+        event.preventDefault();
+        let product = {
+
+            pId: 6,
+            pName: userInput.pName,
+            desc: userInput.pDescription,
+            isAvailable: Boolean(userInput.pAvailability),
+            image: userInput.pImage,
+            price: Number(userInput.pPrice)
+        }
+        console.log(product)
+        // Crear the form - 2 way data binding
+        updateUserInput(
+            {
+                pName: '',
+                pPrice: '',
+                pDescription: '',
+                pAvailability: false,
+                pImage: ''
+            }
+        )
     }
     return (
 
-        <form className="row g-3" >
+        <form className="row g-3" onSubmit={CreateProductEventHandler}>
             <div className="col-md-6">
                 <label for="name">Product Name</label>
                 <input type="text"
                     className="form-control"
                     id="name"
                     placeholder="Product Name"
+                    value={userInput.pName}
                     onChange={nameInputHandler} />
             </div>
             <div className="col-md-6">
@@ -35,6 +104,7 @@ const ProductForm = () => {
                     className="form-control"
                     id="price"
                     placeholder="Product Price"
+                    value={userInput.pPrice}
                     onChange={priceInputHandler} />
             </div>
 
@@ -44,18 +114,19 @@ const ProductForm = () => {
                     className="form-control"
                     id="description"
                     placeholder="Product Description"
+                    value={userInput.pDescription}
                     onChange={descriptionInputHandler} />
             </div>
 
             <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" role="switch" id="isAvailable"
+                <input class="form-check-input" type="checkbox" role="switch" id="isAvailable" checked={userInput.pAvailability}
                     onChange={availabilityInputHandler} />
                 <label class="form-check-label" for="isAvailable">Is product available in stock?</label>
             </div>
 
             <div className="form-group">
                 <label for="select-image">Select product image</label>
-                <input type="file" className="form-control" id="select-image" onChange={imageInputHandler} />
+                <input type="file" className="form-control" id="select-image" value={userInput.pImage} onChange={imageInputHandler} />
             </div>
 
 
