4f212818d41d1c3b28f1ac7a0833cc892f26f4e3
diptu <diptunazmulalam@gmail.com>
2026-05-12 20:19:01 +0600

    feat(day-06):Stateful List and rendering List

--- react-mastery/src/App.jsx
+++ react-mastery/src/App.jsx
@@ -3,18 +3,69 @@ import ProductList from './components/product/ProductList'
 import './App.css'
 import CreateProduct from './components/createProduct/CreateProduct'
 
+import Products from "./components/product/Products"
+import milkImg from './images/fresh-milk.png'
+import cheeseImg from './images/cottage-cheese.png';
+import brocoliImg from './images/brocoli.png';
+import orangesImg from './images/oranges.png';
+import oliveoilImg from './images/olive-oil.png';
+
+let products = [
+  {
+    pID: 1,
+    pName: 'Fresh Milk',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: milkImg,
+    price: 12
+  },
+  {
+    pID: 2,
+    pName: 'Cottage Cheese',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: false,
+    image: cheeseImg,
+    price: 10
+  },
+  {
+    pID: 3,
+    pName: 'Brocoli',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: brocoliImg,
+    price: 15
+  },
+  {
+    pID: 4,
+    pName: 'oranges',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: true,
+    image: orangesImg,
+    price: 20
+  },
+  {
+    pID: 5,
+    pName: 'Olive oil',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
+    isAvailable: false,
+    image: oliveoilImg,
+    price: 14
+  }
+]
+
 
 function App() {
-  let [product, updateProduct] = useState('')
+  let [newProductList, updateNewProductList] = useState(products)
+
   const createProduct = (product) => {
-    updateProduct(product)
+    updateNewProductList([product, ...newProductList])
   }
 
 
   return (
     <>
       <CreateProduct createProduct={createProduct} />
-      <ProductList newProduct={product} />
+      <ProductList newProductList={newProductList} />
     </>
   )
 }
--- react-mastery/src/components/product/ProductList.jsx
+++ react-mastery/src/components/product/ProductList.jsx
@@ -1,84 +1,30 @@
-import Products from "./Products"
-import milkImg from '../../images/fresh-milk.png';
-import cheeseImg from '../../images/cottage-cheese.png';
-import brocoliImg from '../../images/brocoli.png';
-import orangesImg from '../../images/oranges.png';
-import oliveoilImg from '../../images/olive-oil.png';
-let products = [
-    {
-        pID: 1,
-        pName: 'Fresh Milk',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: true,
-        image: milkImg,
-        price: 12
-    },
-    {
-        pID: 2,
-        pName: 'Cottage Cheese',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: false,
-        image: cheeseImg,
-        price: 10
-    },
-    {
-        pID: 3,
-        pName: 'Brocoli',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: true,
-        image: brocoliImg,
-        price: 15
-    },
-    {
-        pID: 4,
-        pName: 'oranges',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: true,
-        image: orangesImg,
-        price: 20
-    },
-    {
-        pID: 5,
-        pName: 'Olive oil',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
-        isAvailable: false,
-        image: oliveoilImg,
-        price: 14
-    }
-]
+import React, { useState } from 'react'
+import Products from './Products'
 
 
 const ProductList = (props) => {
-
-    console.log(props.newProduct)
 
     return (
         <div className='row'>
             <div>
                 <ul class="list-group shadow">
-                    <Products
-                        id={products[0].pID}
-                        name={products[0].pName}
-                        description={products[0].desc}
-                        imgUrl={products[0].image}
-                        price={products[0].price}
-                        isAvailable={products[0].isAvailable}
+                    {
+                        props.newProductList.map(product =>
+                            <Products
+                                id={product?.pID}
+                                name={product?.pName}
+                                description={product?.desc}
+                                imgUrl={product?.image}
+                                price={product?.price}
+                                isAvailable={product.isAvailable}
 
-                    >
-                    </Products>
-                    <Products
-                        id={products[1].pID}
-                        name={products[1].pName}
-                        description={products[1].desc}
-                        imgUrl={products[1].image}
-                        price={products[1].price}
-                        isAvailable={products[1].isAvailable}
+                            ></Products>
 
-                    ></Products>
+                        )};
+
                 </ul>
             </div>
         </div>
-
 
     )
 }
